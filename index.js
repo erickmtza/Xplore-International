e365363869e0c0a83a1772f3b2efc96cefac2cce
Erick Martinez <erick.martinez2013@gmail.com>
2019-02-01 01:25:28 -0600

    partial name search, list of countries will appear

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
+'use strict';
+
+function matchedCountries() {
+    $('.country-opt-dis').on('click', 'input', function() {
+        const chosenCountry = $(this).val();
+        $('.country-options').addClass('hidden');
+        
+        countryData(chosenCountry);
+    })
+}
+
 function displayCountryData(responseJson) {
 
     $('#result-data').empty();
     $('#results').removeClass('hidden');
+    $('#js-error-message').empty();
+    
 
-    $('#result-data').append(
+    if ( responseJson.length > 1 ) {
+        console.log(responseJson);
+        $('.country-options').removeClass('hidden');
+        
+        for (let i = 0; i < responseJson.length; i++) {
+            $('.country-opt-dis').append(
+                `<label for="country_${i}">${responseJson[i].name}</label>
+
+                <input type="radio" name="option" id="country_${i}" value="${responseJson[i].name}" required>`
+            )
+        }
+    } else {
+        $('#result-data').append(
         `
         <img class="country-flag" src="${responseJson[0].flag}" alt="${responseJson[0].name}">
         <h2>${responseJson[0].name}, ${responseJson[0].subregion}</h2>
         `
-    )
+    )}
 }
 
 function countryData(country) {
@@ -37,5 +62,6 @@ $(function searchFetch() {
         const country = $('#country-destination').val();
 
         countryData(country);
+        matchedCountries();
     })
 })
